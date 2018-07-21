14397e0df980fce8076d2444e65594aef474dcab
sandy30716 <sandy30716@gmail.com>
2018-07-21 16:09:31 +0800

    test

--- line-bot-sdk-tiny/echo_bot.php
+++ line-bot-sdk-tiny/echo_bot.php
@@ -22,120 +22,32 @@ foreach ($client->parseEvents() as $event) {
     switch ($event['type']) {
         case 'message':
             $message = $event['message'];
-            $replyMsg = $message['text'];
-            if(strpos($message['text'], '賺錢') !== false){
-                $replyMsg = '近三月績效排行前三名為...';
-            }
             switch ($message['type']) {
                 case 'text':
-                    $client->replyMessage(array(
-                        'replyToken' => $event['replyToken'],
-                        'messages' => array(
-                            array(
-                                'type' => 'text',
-                                'text' => $replyMsg
-                            ),
-                            array(
-                                'type' => 'text',
-                                'text' => '感謝您使用chatvisor服務'
-                            ),
-                            array (
-  'type' => 'template',
-  'altText' => 'this is a carousel template',
-  'template' => 
-  array (
-    'type' => 'carousel',
-    'actions' => 
-    array (
-    ),
-    'columns' => 
-    array (
-      0 => 
-      array (
-        'title' => '復華中小精選基金',
-        'text' => '淨值81.49,近三個月報酬率17.81',
-        'actions' => 
-        array (
-          0 => 
-          array (
-            'type' => 'postback',
-            'label' => '加入觀察清單',
-            'text' => '將ＸＸＸ基金加入網銀觀察清單',
-            'data' => 'shoppingCart',
-          ),
-          1 => 
-          array (
-            'type' => 'postback',
-            'label' => '立即申購',
-            'text' => '立即申購ＸＸＸ基金',
-            'data' => 'buy',
-          ),
-          2 => 
-          array (
-            'type' => 'uri',
-            'label' => '看更多詳細資訊',
-            'uri' => 'https://www.esunbank.com.tw/bank/personal/wealth/fund/ranking',
-          ),
-        ),
-      ),
-      1 => 
-      array (
-        'title' => '統一台灣動力基金',
-        'text' => '淨值81.49,近三個月報酬率17.81',
-        'actions' => 
-        array (
-          0 => 
-          array (
-            'type' => 'message',
-            'label' => '動作 1',
-            'text' => '動作 1',
-          ),
-          1 => 
-          array (
-            'type' => 'message',
-            'label' => '動作 2',
-            'text' => '動作 2',
-          ),
-          2 => 
-          array (
-            'type' => 'message',
-            'label' => '動作 3',
-            'text' => '動作 3',
-          ),
-        ),
-      ),
-      2 => 
-      array (
-        'title' => '復華全方位基金',
-        'text' => '淨值81.49,近三個月報酬率17.81',
-        'actions' => 
-        array (
-          0 => 
-          array (
-            'type' => 'message',
-            'label' => '動作 1',
-            'text' => '動作 1',
-          ),
-          1 => 
-          array (
-            'type' => 'message',
-            'label' => '動作 2',
-            'text' => '動作 2',
-          ),
-          2 => 
-          array (
-            'type' => 'message',
-            'label' => '動作 3',
-            'text' => '動作 3',
-          ),
-        ),
-      ),
-    ),
-  ),
-)
-                         
-                        )
-                    ));
+                    $replyMsg = $message['text'];
+                    if(strpos($message['text'], '賺錢') !== false){
+                        $replyMsg = '近三月績效排行前三名為...';
+                        $client->replyMessage(array(
+                            'replyToken' => $event['replyToken'],
+                            'messages' => array(
+                                array(
+                                    'type' => 'text',
+                                    'text' => $replyMsg
+                                )
+                            )
+                        ));
+                    }
+                    else{
+                        $client->replyMessage(array(
+                            'replyToken' => $event['replyToken'],
+                            'messages' => array(
+                                array(
+                                    'type' => 'text',
+                                    'text' => $replyMsg
+                                )
+                            )
+                        ));
+                    }
                     break;
                 default:
                     error_log("Unsupporeted message type: " . $message['type']);
