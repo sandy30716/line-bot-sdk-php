c2bff22c859c8267c9460d702d01ad672775ba60
Wu Chia-chih <sandy30716@gmail.com>
2018-08-05 16:59:41 +0800

    popular rank1

--- line-bot-sdk-tiny/echo_bot.php
+++ line-bot-sdk-tiny/echo_bot.php
@@ -626,8 +626,8 @@ foreach ($client->parseEvents() as $event) {
                               array(
                                 'type' => 'text',
                                 'text' => emoticon('100077').'你較勇於嘗試新觀念新方法新事物，也願意利用風險較高或是新推出的金融商品作為投資工具，來獲取較高的報酬。'
-                              )//,
-                              //$rank
+                              ),
+                              $popularRank1
                                 
                             )
                         ));
